bba25a0c7095dd7eaa571e8903e5be1b1f312d84
devmustroc <elmustapha.abourar@outlook.com>
2024-12-23 20:34:10 +0100

    :art: setting up the environment

---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export const AuthScreen = () => {
+    return (
+        <div
+            className="h-full flex item-center justify-center bg-amber-700"
+        >
+            <div
+                className="md:h-auto md:w-[420px]"
+            >
+                div
+            </div>
+        </div>
+    );
+}
